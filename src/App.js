cafc51371d0f9a37aedb2d99e5f77ba8c9849dca
iakash1 <akashakc.aa@gmail.com>
2026-08-18 00:43:32 +0530

    React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MovieGrid from "./components/movieGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header></Header>
+        <Router>
+          <nav></nav>
+          <Routes>
+            <Route path="/" element={<MovieGrid />}></Route>
+            <Route path="/watchlist" element={<Watchlist />}></Route>
+          </Routes>
+        </Router>
+      </div>
+
+      <Footer></Footer>
     </div>
   );
 }
